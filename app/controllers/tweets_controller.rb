8fa690432a043503749071b3da675ee6550e8440
yukiraicom <yuukiarai19970524@gmail.com>
2018-06-20 15:10:09 +0900

    つぶやき昨日のビュー

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,16 @@
 class TweetsController < ApplicationController
   def index
+    @tweet = Tweet.new
     @tweets = Tweet.all
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+  
+  private
+  
+  def tweet_params
+    params.permit(:text)
   end
 end
