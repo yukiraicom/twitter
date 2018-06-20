8e9e7c77847e04eb9e75ed2db2777c9cc19f0b57
yukiraicom <yuukiarai19970524@gmail.com>
2018-06-20 11:34:28 +0900

    add tweet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180620022921) do
+
+  create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "tweet"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
