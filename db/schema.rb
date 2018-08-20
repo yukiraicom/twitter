d7faf730e258ccad6e1a6a4f425c1783b25e7f7b
yukiraicom <yuukiarai19970524@gmail.com>
2018-08-20 16:21:54 +0900

    delete name

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20180620051643) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
     t.string   "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
