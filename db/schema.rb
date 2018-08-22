1dc3af0b93cdf251a437a85d712ec23765ed2321
yukiraicom <yuukiarai19970524@gmail.com>
2018-08-22 20:54:55 +0900

    nameをサインアップ時に使えるように変更

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20180620051643) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "nickname"
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
