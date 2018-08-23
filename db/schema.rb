8b403dae4fdb4ac77546b826df6f260181d4c60e
yukiraicom <yuukiarai19970524@gmail.com>
2018-08-23 20:49:59 +0900

    tweetテーブルにuser_id追加・tweetにuser_idを含ませた。

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180823025946) do
+ActiveRecord::Schema.define(version: 20180823112602) do
 
   create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
