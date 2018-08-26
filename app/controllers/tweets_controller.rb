cc8f651c9855e4c3e9686e2e13724ec8930debaf
yukiraicom <yuukiarai19970524@gmail.com>
2018-08-26 16:54:45 +0900

    add gTags method add tags view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,13 +6,26 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
     @tweets = Tweet.all.order("created_at DESC")
     @tweetsnumber = Tweet.where(user_id: current_user.id).length
+    #@tweetTags = Tweet.find()where(tags: )
+
   end
 
   def create
     Tweet.create(tweet_params)
     redirect_to action: :index
   end
-  
+
+  def gTags(tweetId)
+    tweetTags = tweetId
+    tag = Tweet.find(tweetTags)
+    #_by(tags: "id = tweetTags and tags: like '#%'")
+    tag.tags
+    #binding.pry
+
+    return tag.tags
+  end
+
+  helper_method :gTags
   private
   
   def tweet_params
