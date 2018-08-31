7121e8ce6ce7ce15c4143cb55588aa0fa23f9d3e
yukiraicom <yuukiarai19970524@gmail.com>
2018-08-31 17:13:19 +0900

    add get_hashTag method

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,12 @@
 class Tweet < ApplicationRecord
+  before_commit :get_hashTag
   mount_uploader :image, ImageUploader
   belongs_to :user
+
+  def get_hashTag
+    tweet = Tweet.last(1).pluck(:text)
+
+    binding.pry
+    #hashTag = 
+  end
 end
