ce6e42d980274b5576238e0ea0f64e4eb75a368c
yukiraicom <yuukiarai19970524@gmail.com>
2018-09-04 22:06:42 +0900

    タグをクリックしてビューに行く機能。

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,6 +1,15 @@
 class HashtagsController < ApplicationController
   def show
     @tag = Hashtag.find_by("tag = '#{params[:id]}'")
+    @tags = Hashtag.where("tag = '#{params[:id]}'")
+
+    #binding.pry
+    def tweetText(tweetText)
+      tweet = tweetText.sub(/#.*/, "")
+      return tweet
+    end
+    
   end
+  helper_method :tweetText
 end
 
