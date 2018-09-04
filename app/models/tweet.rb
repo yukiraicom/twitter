a5b3b6e6affcc8c2e19653d53393c5cfb17a547b
yukiraicom <yuukiarai19970524@gmail.com>
2018-09-04 12:53:42 +0900

    ハッシュタグをビューにaタグで表示

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
   before_commit :get_hashTag
   mount_uploader :image, ImageUploader
   belongs_to :user
-  has_many :tweets
+  has_many :tags
 
   def get_hashTag
     tweet = Tweet.pluck(:text).last
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902123626) do
+ActiveRecord::Schema.define(version: 20180904021935) do
 
   create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "tag"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20180902123626) do
     t.datetime "updated_at", null: false
     t.string   "image"
     t.integer  "user_id"
-    t.string   "tags"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
