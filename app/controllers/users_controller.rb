5b7b43705392c0fdbf0c0209c99ba01a39bbec63
yukiraicom <yuukiarai19970524@gmail.com>
2018-09-07 16:24:23 +0900

    make user#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @tweets = @tweets = Tweet.where(user_id: params[:id]).order("created_at DESC")
+    
   end
+
+  def tweetText(tweetText)
+    tweet = tweetText.sub(/#.*/, "")
+    return tweet
+  end
+
+
+  # def gTags(tweetId)
+  #   tweetTags = Tag.where(tweet_id: :tweetId)
+  #   return tweetTags
+  # end
+
+  helper_method :tweetText
+  helper_method :gTags
 end
