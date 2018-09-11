8316ff03a4982404798c1784b96775f1c27a0fa4
yukiraicom <yuukiarai19970524@gmail.com>
2018-09-11 17:48:02 +0900

    フォロー機能

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,16 @@
 class FollowsController < ApplicationController
+  def create
+    # binding.pry
+    # follow = Follow.find_by(follow_id: "current_user.id")
+    # if follow.
+    Follow.create(user_id: follow_params, follow_id: current_user.id)
+    # binding.pry
+
+  end
+
+  private
+  def follow_params
+    params.require(:user_id)
+    # binding.pry
+  end
 end
