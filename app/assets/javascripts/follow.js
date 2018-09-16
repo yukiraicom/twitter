7f172a9a914c7faf628751ab0505924e222e80ed
yukiraicom <yuukiarai19970524@gmail.com>
2018-09-16 18:57:23 +0900

    フォロー解除時赤にした

--- app/assets/javascripts/follow.js
+++ app/assets/javascripts/follow.js
@@ -1,9 +1,13 @@
 $(document).on('turbolinks:load', function() {
   $('.unfollow').mouseover(function(){
     $('.unfollow').val("フォロー解除");
+    $('.unfollow').css('background-color', 'red');
+
   });
   $('.unfollow').mouseout(function(){
     $(".unfollow").val("フォロー中");
+    $('.unfollow').css('background-color', '#49a9d4');
+
   })
   $('.unfollow').click(function(){
     prop('disabled', false);
