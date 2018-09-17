0e1941aed8d4c81a6b0476373b6f3820e0ebd370
yukiraicom <yuukiarai19970524@gmail.com>
2018-09-17 11:15:44 +0900

    フォローボタンの色調整

--- app/assets/javascripts/follow.js
+++ app/assets/javascripts/follow.js
@@ -1,4 +1,8 @@
 $(document).on('turbolinks:load', function() {
+
+  $('.unfollow').css('background-color', '#49a9d4');
+  $('.unfollow').css('color', '#FFF');
+
   $('.unfollow').mouseover(function(){
     $('.unfollow').val("フォロー解除");
     $('.unfollow').css('background-color', 'red');
@@ -7,7 +11,7 @@ $(document).on('turbolinks:load', function() {
   $('.unfollow').mouseout(function(){
     $(".unfollow").val("フォロー中");
     $('.unfollow').css('background-color', '#49a9d4');
-
+    $('.unfollow').css('color', '#FFF');
   })
   $('.unfollow').click(function(){
     prop('disabled', false);
@@ -18,10 +22,14 @@ $(document).on('turbolinks:load', function() {
   });
 
   $('.follow').mouseover(function(){
+    $('.follow').css('background-color', '#49a9d4');
+    $('.follow').css('color', '#FFF');
     $('.follow').val("フォローする");
   });
 
   $('.follow').mouseout(function(){
+    $('.follow').css('background-color', '#FFF');
+    $('.follow').css('color', '#49a9d4');
     $(".follow").val("フォロー");
   });
 });
