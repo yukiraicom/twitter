2442fcf0a48b3f6b101f6ef253f914960e03e815
yukiraicom <yuukiarai19970524@gmail.com>
2018-10-14 11:21:06 +0900

    add dependent: :destroy to favorite

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,7 @@ class Tweet < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :hashtags
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
 
   def get_hashTag
     tweet = Tweet.pluck(:text).last
