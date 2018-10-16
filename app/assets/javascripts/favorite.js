e86e9f69106503a7653a3bc0ca5bca7467ea6f9c
yukiraicom <yuukiarai19970524@gmail.com>
2018-10-16 15:04:33 +0900

    ajaxでいいねをデータベースへ viewにはまだ反映してない

--- app/assets/javascripts/favorite.js
+++ app/assets/javascripts/favorite.js
@@ -1,6 +1,18 @@
-// $(document).on('turbolinks:load', function() {
-//   $('.fa-heart-o').on("click"
-//   //いいねしている時としていない時の判別を作ったら実装
-  
-//   )
-// }
+$(document).on('turbolinks:load', function() {
+  $('.fa-heart-o').on("click",function(){
+  var tweet = $('.tweet-area__boader').first().data("id");
+  console.log(tweet);
+    $.ajax({
+      type: "POST",
+      url: '/favorites',
+      data: {id: tweet},
+      dataType: "json"
+    })
+    .done(function(data){
+      $(this).addClass('icon-red');
+    })
+    .fail(function(){
+      alert('fail');
+    })
+  })
+})
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,8 @@
 class FavoritesController < ApplicationController
   def create
-    
+    tweetId = params.require(:id).to_i
+    Favorite.create(user_id: current_user.id, tweet_id: tweetId)
+    # //カスタムデータ属性でtweet.idを引っ張る
   end
 end
   
