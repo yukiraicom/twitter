0266174d3d116e4528c981ecf06c4bae8c17330f
yukiraicom <yuukiarai19970524@gmail.com>
2018-10-24 11:28:44 +0900

    開発中にいいねされまくるからコメントアウト

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,7 @@
 class FavoritesController < ApplicationController
   def create
-    tweetId = params.require(:id).to_i
-    Favorite.create(user_id: current_user.id, tweet_id: tweetId)
-    # //カスタムデータ属性でtweet.idを引っ張る
+    # tweetId = params.require(:id).to_i
+    # Favorite.create(user_id: current_user.id, tweet_id: tweetId)
   end
 
   def destroy
