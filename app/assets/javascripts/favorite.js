4c908a3a8c1eff88cb54499d14c14fb2541e55e1
yukiraicom <yuukiarai19970524@gmail.com>
2018-10-26 16:00:59 +0900

    favoriteをdatabaseとviewに反映

--- app/assets/javascripts/favorite.js
+++ app/assets/javascripts/favorite.js
@@ -11,7 +11,8 @@ $(document).on('turbolinks:load', function() {
       .done(function(data){
         $('#red').removeClass('fa fa-heart-o');
         $('#red').addClass('fa fa-heart fa-icon-red'); 
-        console.log("done")
+        $('#red').prop('id', '')
+        console.log("add red done")
       })
       .fail(function(){
         alert('fail');
@@ -20,8 +21,21 @@ $(document).on('turbolinks:load', function() {
 
   $(".tweet-area__action-area").on("click", ".fa-icon-red", function(){
     $(this).prop('id', 'unred')
-    $('#unred').removeClass('fa fa-heart fa-icon-red');
-    $('#unred').addClass('fa fa-heart-o');
+    var tweet = $(this).parent().parent().data("id");
+    $.ajax({
+      type: "DELETE",
+      url: `/favorites/${tweet}`,
+      dataType: "json"
+    })
+    .done(function(data){
+      console.log("add unred done")
+      $('#unred').removeClass('fa fa-heart fa-icon-red');
+      $('#unred').addClass('fa fa-heart-o');
+      $('#unred').prop('id', '')
+    })
+    .fail(function(){
+      alert('fail');
+    })
   })
 
   
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,12 @@
 class FavoritesController < ApplicationController
   def create
-    # tweetId = params.require(:id).to_i
-    # Favorite.create(user_id: current_user.id, tweet_id: tweetId)
+    tweetId = params.require(:id).to_i
+    Favorite.create(user_id: current_user.id, tweet_id: tweetId)
   end
 
   def destroy
+    tweetId = params.require(:id).to_i
+    favorite = Favorite.find_by(user_id: current_user.id, tweet_id: tweetId)
+    favorite.destroy if favorite
   end
 end
-  
