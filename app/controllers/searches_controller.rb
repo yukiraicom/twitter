14bd90e7ea2a34588dc61a6e28221796d0d1606f
yukiraicom <yuukiarai19970524@gmail.com>
2018-10-28 18:15:52 +0900

    params受け取り

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,5 +2,10 @@ class SearchesController < ApplicationController
   def index
   end
   def search
+    searchWord = search_params
+  end
+
+  def search_params
+    params.require(:word)[:name]
   end
 end
